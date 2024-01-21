57f8c97aa61cd58bd5b84487cb01514ed3816fc5
Keith Mancuso <keithmancuso@gmail.com>
2024-01-21 14:37:51 -0800

    add support for filtered get lists

--- pages/api/shows/index.ts
+++ pages/api/shows/index.ts
@@ -3,8 +3,11 @@ import { sql } from '@vercel/postgres';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
-    const { rows: shows } = await sql`SELECT id, name, app, status FROM shows`;
-    res.status(200).json({ shows });
+    const status = typeof req.query.status === 'string' ? req.query.status : undefined;
+    const { rows: shows } = status
+    ? await sql`SELECT id, name, app, status FROM shows WHERE status = ${status}`
+    : await sql`SELECT id, name, app, status FROM shows`;
+      res.status(200).json({ shows });
   } else if (req.method === 'POST') {
     const { name, app, status } = req.body;
     await sql`INSERT INTO shows (name, app, status) VALUES (${name}, ${app}, ${status})`;
