b3a3266d73576ce9d80628f8ad41783b896848ef
Keith Mancuso <keithmancuso@gmail.com>
2024-01-21 12:43:20 -0800

    cleanup api calls to get basics working

--- app/api/shows/index.ts
+++ app/api/shows/index.ts
@@ -1,18 +1,19 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { withPostgres, QueryClient } from '@vercel/postgres';
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-async function handler(req: NextApiRequest, res: NextApiResponse, client: QueryClient) {
-    if (req.method === 'POST') {
-        const { title, app, status } = req.body;
-        await client.query('INSERT INTO shows (title, app, status) VALUES ($1, $2, $3)', [title, app, status]);
-        return res.status(201).json({ message: 'Show added' });
-    } else if (req.method === 'GET') {
-        const { rows } = await client.query('SELECT * FROM shows');
-        return res.status(200).json(rows);
-    } else {
-        res.setHeader('Allow', ['GET', 'POST']);
-        return res.status(405).end(`Method ${req.method} Not Allowed`);
+
+export async function handler(request: Request) {
+    if (request.method === 'POST') {
+        const { title, app, status } = request.body;
+        await sql`INSERT INTO shows (title, app, status) VALUES ($1, $2, $3)', [title, app, status]`;
+
+        return NextResponse.json({ message: 'Show added'  }, { status: 200 });
+
+    } else if (request.method === 'GET') {
+        const shows = await sql`SELECT * FROM shows`;
+        
+        return NextResponse.json({ shows }, { status: 200 });
+
     }
 }
 
-export default withPostgres(handler);
