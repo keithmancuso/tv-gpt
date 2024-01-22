fe06985d8fdb9b63640d76517a748a13f0e2904f
Keith Mancuso <keithmancuso@gmail.com>
2024-01-21 22:15:09 -0800

    add support for rating, reviews and favorite shows

--- pages/api/shows/index.ts
+++ pages/api/shows/index.ts
@@ -5,12 +5,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (req.method === 'GET') {
     const status = typeof req.query.status === 'string' ? req.query.status : undefined;
     const { rows: shows } = status
-    ? await sql`SELECT id, name, app, status FROM shows WHERE status = ${status}`
-    : await sql`SELECT id, name, app, status FROM shows`;
+    ? await sql`SELECT id, name, app, status, rating, review, favorite FROM shows WHERE status = ${status}`
+    : await sql`SELECT id, name, app, status, rating, review, favorite FROM shows`;
       res.status(200).json({ shows });
   } else if (req.method === 'POST') {
-    const { name, app, status } = req.body;
-    await sql`INSERT INTO shows (name, app, status) VALUES (${name}, ${app}, ${status})`;
+    const { name, app, status, rating, review, favorite } = req.body;
+    await sql`INSERT INTO shows (name, app, status, rating, review, favorite) VALUES (${name}, ${app}, ${status}, ${rating}, ${review}, ${favorite})`;
     res.status(201).json({ message: 'Show added' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
