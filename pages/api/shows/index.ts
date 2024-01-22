39cda0805c4befdd94ac8019fafe588b357fe1c5
Keith Mancuso <keithmancuso@gmail.com>
2024-01-22 08:21:13 -0800

    cleanup

--- pages/api/shows/index.ts
+++ pages/api/shows/index.ts
@@ -17,40 +17,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   } else if (req.method === 'PUT') {
 
-        if (!id) {
-          res.status(400).json({ message: 'Invalid show ID' });
-          return;
-        }
-        
-        let fields="";
-        if (title) {
-            fields += `SET title = ${title}`
-        }
-        if (app) {
-            fields += `SET app = ${app}`
-        }
-        if (status) {
-            fields += `SET status = ${status}`
-        }
-        if (rating) {
-            fields += `SET rating = ${rating}`
-        }
-        if (review) {
-            fields += `SET review = ${review}`
-        }
-        if (favorite) {
-            fields += `SET favorite = ${favorite}`
-        }
-        await sql`UPDATE shows `+fields + `WHERE id = ${id}`;
-        res.status(200).json({ message: 'Show updated' });
-    } else if (req.method === 'DELETE') {
-        if (!id) {
-          res.status(400).json({ message: 'Invalid show ID' });
-          return;
-        } 
-        await sql`DELETE FROM shows WHERE id = ${id}`;
-        res.status(200).json({ message: 'Show deleted' });
-    } else {
+    if (!id) {
+      res.status(400).json({ message: 'Missing show ID' });
+      return;
+    }
+    
+    let fields="";
+    if (title) {
+        fields += `SET title = ${title}`
+    }
+    if (app) {
+        fields += `SET app = ${app}`
+    }
+    if (status) {
+        fields += `SET status = ${status}`
+    }
+    if (rating) {
+        fields += `SET rating = ${rating}`
+    }
+    if (review) {
+        fields += `SET review = ${review}`
+    }
+    if (favorite) {
+        fields += `SET favorite = ${favorite}`
+    }
+    await sql`UPDATE shows `+fields + `WHERE id = ${id}`;
+    res.status(200).json({ message: 'Show updated' });
+  } else if (req.method === 'DELETE') {
+    if (!id) {
+      res.status(400).json({ message: 'Missing show ID' });
+      return;
+    } 
+    await sql`DELETE FROM shows WHERE id = ${id}`;
+    res.status(200).json({ message: 'Show deleted' });
+  } else {
     res.status(405).json({ message: 'Method not allowed' });
   } 
 }
