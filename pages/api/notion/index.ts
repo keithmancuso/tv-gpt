291625ea9603527080b2dc26af93fbcd73edc89b
Keith Mancuso <keithmancuso@gmail.com>
2024-02-04 12:02:52 -0800

    add notion endpoint to get shows

---
+++ pages/api/notion/index.ts
@@ -0,0 +1,81 @@
+// pages/api/queryNotion.ts
+
+import { Client } from '@notionhq/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN, // Make sure to add your Notion token to your environment variables
+});
+
+const databaseId = process.env.WATCHING_DATABASE; // Your Notion Database ID also stored as an environment variable
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+   // Extract status from query parameters or default to 'Watching'
+   const { status } = req.body || {};
+
+  try {
+    // Construct the query with a condition for the status filter
+    const query: any = {
+      database_id: databaseId,
+    };
+
+    if (status) {
+      query.filter = {
+        property: 'Status',
+        select: {
+          equals: status,
+        },
+      };
+    };
+
+    const response = await notion.databases.query(query);
+
+
+
+     // Process and simplify the results
+     const simplifiedResults = response.results.map((page) => {
+      return {
+        Name: page.properties.Name.title[0]?.plain_text || 'No Name',
+        App: page.properties.App?.select?.name || 'No App',
+        Status: page.properties.Status?.select?.name || 'No Status'
+      };
+    });
+
+    // Define the type for a single result
+    interface Result {
+      Name: string;
+      App: string;
+      Status: string;
+    }
+
+    // Define the type for the grouped results
+    interface GroupedResults {
+      [key: string]: Result[];
+    }
+
+    // Group the results by status
+    const groupedByStatus = simplifiedResults.reduce<GroupedResults>((acc, curr) => {
+      // Use the status as the key for grouping
+      const key = curr.Status;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+
+      delete curr.Status; // Remove the status from the result
+      acc[key].push(curr);
+      return acc;
+    }, {});
+
+
+    res.status(200).json(groupedByStatus);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to query Notion database' });
+  }
+}
