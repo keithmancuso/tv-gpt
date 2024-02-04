72786fc3590170f08b63b8c082acd9f6919c50de
Keith Mancuso <keithmancuso@gmail.com>
2024-02-04 13:51:20 -0800

    fix get results

--- pages/api/notion/index.js
+++ pages/api/notion/index.js
@@ -69,7 +69,7 @@ export default async function handler(req, res) {
       return acc;
     }, {});
 
-    res.status(200).json(response.results);
+    res.status(200).json(groupedResults);
 
   } catch (error) {
     console.error(error);
