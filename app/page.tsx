b35baa3a43e2358bcbd0ae0d945ab1dc7b88fc65
Keith Mancuso <keithmancuso@gmail.com>
2024-08-29 12:16:55 -0700

    building again

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import NewBtn from '@/components/new'
 import Remove from '@/components/remove'
 import Link from 'next/link';
 
+
+
 export default async function Home({
   searchParams,
 }: {
@@ -15,14 +17,13 @@ export default async function Home({
 
   const statuses = ['Watching', 'Next', 'Loved'];
 
+
   return (
     <>
       <div className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-6 dark:border-white/10">
         <Heading>{status}</Heading>
         <NewBtn/>
       </div>
-
-    
 
       <Table>
         <TableHead>
@@ -35,7 +36,9 @@ export default async function Home({
         <TableBody>
           {shows.map((show) => (
             <TableRow key={show.id}>
-              <TableCell className="font-medium">{show.name}</TableCell>
+              <TableCell className="font-medium">
+                {show.name}
+              </TableCell>
               <TableCell>{show.app}</TableCell>
               <TableCell>
                 <Remove id={show.id} />
